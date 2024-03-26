feebfb556ffa39a826a54689d80539067df8f529
gerardjr42 <ggarciajr12@gmail.com>
2024-03-26 01:21:57 -0400

    Add dynamic values for Progress component

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,38 @@ import Progress from "./Components/Progress";
 import RecentDonations from "./Components/RecentDonations";
 
 const targetAmount = 1000;
+const donations = [
+  {
+    amount: 250,
+    caption: "You really need this. Really.",
+    id: 1,
+    name: "Jo",
+  },
+  {
+    amount: 30,
+    caption: "Here, take a break from work!",
+    id: 2,
+    name: "Rami",
+  },
+  {
+    amount: 20,
+    caption: "LOL! You are too funny. Happy to do this for you. :)",
+    id: 3,
+    name: "Michelle",
+  },
+  {
+    amount: 5,
+    caption: "Have fun!",
+    id: 4,
+    name: "Malinda",
+  },
+  {
+    amount: 30,
+    caption: "Come visit me in Miami!",
+    id: 5,
+    name: "Sam",
+  },
+];
 
 function App() {
   return (
@@ -15,10 +47,10 @@ function App() {
       <TopBar />
       <main className="container">
         <section className="sidebar">
-          <RecentDonations/>
+          <RecentDonations donations = {donations}/>
         </section>
         <section className="">
-          <Progress/>
+          <Progress  donations={donations} targetAmount={targetAmount}/>
           <DonationForm/>
         </section>
       </main>
--- src/Components/Progress.jsx
+++ src/Components/Progress.jsx
@@ -1,8 +1,11 @@
-const Progress = ({currentAmount, targetAmount}) => {
+const Progress = ({targetAmount, donations}) => {
+  
+  const currentAmount = donations.reduce((acc, curr) => acc + curr.amount, 0);
+
   return (
     <section className="progress">
       <h2>
-        Raised <span className="secondary">{currentAmount}</span> of <span className="secondary">{targetAmount}</span>
+        Raised <span className="secondary">${currentAmount}</span> of <span className="secondary">${targetAmount}</span>
       </h2>
     </section>
   )
