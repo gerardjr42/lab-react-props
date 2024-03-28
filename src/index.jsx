4c7e004efd918ac691357429579d345f86ba32ae
gerardjr42 <ggarciajr12@gmail.com>
2024-03-28 05:34:03 -0400

    Remove strictMode for production

--- src/index.jsx
+++ src/index.jsx
@@ -6,7 +6,5 @@ import { StrictMode } from 'react';
 
 // ReactDOM.createRoot(document.getElementById("root")).render(<App />);
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <StrictMode>
     <App />
-  </StrictMode>
 );
